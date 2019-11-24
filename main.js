1cf3322d072f1133babbb72d54547d9f0b4a0b01
Tambini <tambini.c@gmail.com>
2019-11-24 17:04:58 -0500

    Added images, and made baloon move oen page on refresh

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
+
 window.onload = function () {
 
+  // First I set up the api to verify I could use it  
 
-  const apiUrl = 'https://api.tronalddump.io/random/quote';
-  const button = document.querySelector("button")
+  // const apiUrl = 'https://api.tronalddump.io/random/quote';
+  // const button = document.querySelector("button")
 
-  button.addEventListener("click", async function (evt) {
-    evt.preventDefault();
-    const result = await axios.get(`${apiUrl}`, {
-      headers: { 'accept': 'application/hal+json' }
-    })
-    console.log(result)
-  })
+  // button.addEventListener("click", async function (evt) {
+  //   evt.preventDefault();
+  //   const result = await axios.get(`${apiUrl}`, {
+  //     headers: { 'accept': 'application/hal+json' }
+  //   })
+  //   console.log(result)
+  // })
+
+  //Now i'm going to create the ballon to show up on the page
+
+
+  function addBalloon() {
+    const balloon = document.createElement("div");
+    balloon.classList.add("balloon");
+    const body = document.querySelector("body")
+    body.appendChild(balloon);
+    console.log(body)
+  }
+  addBalloon()
+
+  function moveballoon() {
+    const balloon = document.querySelector(".balloon");
+    balloon.style.left = Math.random() * window.innerWidth + "px";
+    balloon.style.top = Math.random() * window.innerHeight + "px";
+  }
+
+  moveballoon()
+
+
 }
 
