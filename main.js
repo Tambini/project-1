c641287bcc20b0f0f81e3643f0e0609a6988edda
Tambini <tambini.c@gmail.com>
2019-11-25 14:07:55 -0500

    adding click functionality

--- main.js
+++ main.js
@@ -3,72 +3,15 @@ window.onload = function () {
 
   // First I set up the api to verify I could use it  
 
-  // const apiUrl = 'https://api.tronalddump.io/random/quote';
-  // const button = document.querySelector("button")
-
-  // button.addEventListener("click", async function (evt) {
-  //   evt.preventDefault();
-  //   const result = await axios.get(`${apiUrl}`, {
-  //     headers: { 'accept': 'application/hal+json' }
-  //   })
-  //   console.log(result)
-  // })
-
-  // //Now i'm going to create the ballon to show up on the page
-
-
-  // function addBalloon() {
-  //   const balloon = document.createElement("div");
-  //   balloon.classList.add("balloon");
-  //   const body = document.querySelector("body")
-  //   body.appendChild(balloon);
-  //   console.log(body)
-  // }
-  // addBalloon()
-
-  // // now I will make the balloon move on the page on refresh
-
-  // // function moveBalloon() {
-  // //   const balloon = document.querySelector(".balloon");
-  // //   // balloon.style.bottom = Math.random() * window.innerWidth + "px";
-  // //   balloon.style.left = Math.random() * window.innerHeight + "px";
-  // //   balloon.style.top = Math.random() * window.innerHeight + "px";
-  // // }
-  // // moveBalloon()
-
-  // // now I need to make the balloon move all over the page without
-  // // having to hit refresh
-
-  // function moveBalloonObject() {
-  //   const balloon = document.querySelector(".balloon");
-  //   // obj.style.bottom = Math.random() * window.innerWidth + "px";
-  //   balloon.style.left = Math.random() * window.innerHeight + "px";
-  //   balloon.style.top = Math.random() * window.innerHeight + "px";
-  // };
-
-  // setInterval(function () {
-  //   moveBalloonObject(document.querySelector(".balloon"));
-  // }, 2000);
-
-  // function multipleBalloons(number) {
-  //   for (let i = 0; i < number; i++) {
-  //     // const balloon = document.querySelector(".balloon")
-  //     const balloon = addBalloon();
-  //     // moveBalloon(balloon);
-  //     moveBalloonObject(balloon);
-  //     setInterval(function () {
-  //       moveBalloonObject(document.querySelector(".balloon"));
-  //     }, 2000);
-  //   }
-  // }
-
-  // multipleBalloons(5);
+  const apiUrl = 'https://api.tronalddump.io/random/quote';
+  const button = document.querySelector("button")
 
 
 
-  // I have to create a function that does steps 1-4 
+  // I used the duck hunt excercise as a guide to build my page
+  //Now i'm going to create the ballon to show up on the page
 
-  function createBalloon() {
+  function addBalloon() {
     const body = document.querySelector("body")
     const balloon = document.createElement("div")
     balloon.classList.add("balloon");
@@ -88,18 +31,40 @@ window.onload = function () {
 
   }
 
-  createBalloon()
+  addBalloon()
 
 
   // /* now I will create a function that creates more than one balloon */
 
   function multipleBalloons(number) {
     for (let i = 0; i < number; i++) {
-      createBalloon()
+      addBalloon()
     }
   }
-  multipleBalloons(5)
+  multipleBalloons(10)
 
+
+  // now I have to add an event handler so when you click on the click event handler you 
+
+  function pop() {
+    let balloons = document.querySelectorAll(".balloon")
+    for (let i = 0; i < balloons.length; i++) {
+      balloons[i].addEventListener("click", function () {
+        this.classList.add("explode");
+        console.log("clicked")
+      })
+    }
+
+  }
+  pop();
+
+ // balloons.addEventListener("click", async function (evt) {
+    //   evt.preventDefault();
+    //   const result = await axios.get(`${apiUrl}`, {
+    //     headers: { 'accept': 'application/hal+json' }
+    //   })
+    //   console.log(result)
+    // })
 
 
 
