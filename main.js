78beb90e4bea67838d598558865ace6e824028da
Tambini <tambini.c@gmail.com>
2019-11-25 12:07:02 -0500

    added landing page and made multiple balloons move

--- main.js
+++ main.js
@@ -14,42 +14,113 @@ window.onload = function () {
   //   console.log(result)
   // })
 
-  //Now i'm going to create the ballon to show up on the page
+  // //Now i'm going to create the ballon to show up on the page
 
 
-  function addBalloon() {
-    const balloon = document.createElement("div");
-    balloon.classList.add("balloon");
+  // function addBalloon() {
+  //   const balloon = document.createElement("div");
+  //   balloon.classList.add("balloon");
+  //   const body = document.querySelector("body")
+  //   body.appendChild(balloon);
+  //   console.log(body)
+  // }
+  // addBalloon()
+
+  // // now I will make the balloon move on the page on refresh
+
+  // // function moveBalloon() {
+  // //   const balloon = document.querySelector(".balloon");
+  // //   // balloon.style.bottom = Math.random() * window.innerWidth + "px";
+  // //   balloon.style.left = Math.random() * window.innerHeight + "px";
+  // //   balloon.style.top = Math.random() * window.innerHeight + "px";
+  // // }
+  // // moveBalloon()
+
+  // // now I need to make the balloon move all over the page without
+  // // having to hit refresh
+
+  // function moveBalloonObject() {
+  //   const balloon = document.querySelector(".balloon");
+  //   // obj.style.bottom = Math.random() * window.innerWidth + "px";
+  //   balloon.style.left = Math.random() * window.innerHeight + "px";
+  //   balloon.style.top = Math.random() * window.innerHeight + "px";
+  // };
+
+  // setInterval(function () {
+  //   moveBalloonObject(document.querySelector(".balloon"));
+  // }, 2000);
+
+  // function multipleBalloons(number) {
+  //   for (let i = 0; i < number; i++) {
+  //     // const balloon = document.querySelector(".balloon")
+  //     const balloon = addBalloon();
+  //     // moveBalloon(balloon);
+  //     moveBalloonObject(balloon);
+  //     setInterval(function () {
+  //       moveBalloonObject(document.querySelector(".balloon"));
+  //     }, 2000);
+  //   }
+  // }
+
+  // multipleBalloons(5);
+
+
+
+  // I have to create a function that does steps 1-4 
+
+  function createBalloon() {
     const body = document.querySelector("body")
+    const balloon = document.createElement("div")
+    balloon.classList.add("balloon");
     body.appendChild(balloon);
-    console.log(body)
+    console.log(body);
+
+    function randomPosition() {
+      balloon.style.left = Math.random() * window.innerWidth + "px";
+      balloon.style.top = Math.random() * window.innerHeight + "px";
+    }
+
+    randomPosition()
+
+    setInterval(randomPosition, 2000);
+
+    return balloon;
+
   }
-  addBalloon()
 
-  // now I will make the balloon move on the page on refresh
+  createBalloon()
 
-  function moveballoon() {
-    const balloon = document.querySelector(".balloon");
-    // balloon.style.bottom = Math.random() * window.innerWidth + "px";
-    balloon.style.left = Math.random() * window.innerHeight + "px";
-    balloon.style.top = Math.random() * window.innerHeight + "px";
+
+  // /* now I will create a function that creates more than one balloon */
+
+  function multipleBalloons(number) {
+    for (let i = 0; i < number; i++) {
+      createBalloon()
+    }
   }
-  moveballoon()
+  multipleBalloons(5)
 
-  // now I need to make the balloon move all over the page without
-  // having to hit refresh
-  const moveBalloonObject = obj => {
-    // obj.style.bottom = Math.random() * window.innerWidth + "px";
-    obj.style.left = Math.random() * window.innerHeight + "px";
-    obj.style.top = Math.random() * window.innerHeight + "px";
-  };
 
-  setInterval(function () {
-    moveBalloonObject(document.querySelector(".balloon"));
-  }, 250);
 
-  moveballoon()
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
